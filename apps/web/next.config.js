1ace0c9e92a4145daf24a19715938d8a570508b3
Bruno Castelo <brunoalexcastelo@gmail.com>
2025-09-05 18:17:28 +0100

    fix: update Dockerfile CMD to use npm start and remove standalone output from next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,9 +1,6 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
-	reactStrictMode: true,
-	assetPrefix: '.',
     async rewrites() {
 		return [
 			{
